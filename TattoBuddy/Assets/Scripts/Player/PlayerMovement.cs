d94a1a6390ee7c45be190473d6194f2cca4d29b9
Semih Uçar <smhucr10@gmail.com>
2023-09-06 04:47:59 +0300

    ChangeScriptNameToSpecific

--- TattoBuddy/Assets/Scripts/Player/PlayerMovement.cs
+++ TattoBuddy/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     private DynamicJoystick joystick;
     private Rigidbody2D rb2D;
