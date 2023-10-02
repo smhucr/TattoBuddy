09c0c067f811fb540d4a104d5e1597894558cab1
Semih Uçar <smhucr10@gmail.com>
2023-10-02 13:08:27 +0300

    MeleeAttackAnimation

--- TattoBuddy/Assets/Scripts/Weapon/SlowMeleeWeapon.cs
+++ TattoBuddy/Assets/Scripts/Weapon/SlowMeleeWeapon.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class SlowMeleeWeapon : MainWeapon
 {
     private void Awake()
     {
-        shootingInterval = 0.7f;
+        shootingInterval = 3f;
         detectionRange = 6.0f;
         attackRange = 4.0f;
     }
@@ -14,7 +14,20 @@ public class SlowMeleeWeapon : MainWeapon
 
     public override void ShootAtTarget()
     {
-        
-        throw new System.NotImplementedException();
+        AttackAnimation();
+    }
+
+    public void AttackAnimation()
+    {
+        float startXRotation = transform.eulerAngles.x;
+        float startYRotation = transform.eulerAngles.y;
+        float startZRotation = transform.eulerAngles.z;
+        transform.DORotate(new Vector3(startXRotation, startYRotation, startZRotation - 360), 1f, RotateMode.FastBeyond360).SetEase(Ease.Linear).OnComplete(() => ToNormalRotation());
+    }
+
+    public void ToNormalRotation()
+    {
+        if (currentTarget == null)
+            transform.rotation = Quaternion.Euler(Vector3.zero);
     }
 }
