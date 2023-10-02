5145acf0968ed3575e3a9ba8c26facbc60e25a97
Semih Uçar <smhucr10@gmail.com>
2023-10-02 18:00:34 +0300

    ImageTearingSolved

--- TattoBuddy/Assets/Scripts/Weapon/SlowMeleeWeapon.cs
+++ TattoBuddy/Assets/Scripts/Weapon/SlowMeleeWeapon.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 using DG.Tweening;
 public class SlowMeleeWeapon : MainWeapon
 {
+    public bool isAttacking = false;
+    public Collider2D colliderWeapon;
     private void Awake()
     {
         shootingInterval = 3f;
         detectionRange = 6.0f;
         attackRange = 4.0f;
+        colliderWeapon = GetComponent<Collider2D>();
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy") && isAttacking)
+        {
+            print("SlowMelee Hitted ::: " + collision.name);
+        }
+    }
 
     public override void ShootAtTarget()
     {
@@ -20,6 +30,8 @@ public class SlowMeleeWeapon : MainWeapon
 
     public void AttackAnimation()
     {
+        targettingInterval = 0.8f; // Temporary Solution for Image Tearing
+        isAttacking = true;
         var tempWeaponPosition = transform.localPosition;
         Vector3 newPosition = Vector3.MoveTowards(transform.position, currentTarget.position, 0.15f);
         transform.position = newPosition;
@@ -29,23 +41,42 @@ public class SlowMeleeWeapon : MainWeapon
         float startZRotation = transform.eulerAngles.z;
         transform.rotation = Quaternion.Euler(new Vector3(startXRotation, startYRotation, startZRotation + 90));
         if (direction.y > 0)
+        {
             transform.DORotate(new Vector3(startXRotation, startYRotation, startZRotation - 120), 0.8f, RotateMode.FastBeyond360).SetEase(Ease.Linear).OnComplete(() =>
-            {
-                ToNormalRotation();
-                transform.localPosition = tempWeaponPosition;
-            });
+                        {
+                            transform.localPosition = tempWeaponPosition;
+                            ToNormalRotation();
+                        });
+            StartCoroutine(ColliderCloseOpen());
+        }
+
         else
+        {
             transform.DORotate(new Vector3(startXRotation, startYRotation, startZRotation - 80), 0.6f).SetEase(Ease.Linear).OnComplete(() =>
-            {
-                ToNormalRotation();
-                transform.localPosition = tempWeaponPosition;
-            });
+                        {
+                            transform.localPosition = tempWeaponPosition;
+                            ToNormalRotation();
+                        });
+            StartCoroutine(ColliderCloseOpen());
+        }
+
 
     }
 
     public void ToNormalRotation()
     {
         if (currentTarget == null)
+        {
             transform.rotation = Quaternion.Euler(Vector3.zero);
+        }
+        targettingInterval = 0.2f;
+        isAttacking = false;
+    }
+
+    IEnumerator ColliderCloseOpen()
+    {
+        colliderWeapon.enabled = true;
+        yield return new WaitForSeconds(1f);
+        colliderWeapon.enabled = false;
     }
 }
