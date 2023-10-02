3fcb508601da6ef02cd310b7dc8789ca0dbe60c5
Semih Uçar <smhucr10@gmail.com>
2023-10-02 16:53:12 +0300

    BetterAnimCodes

--- TattoBuddy/Assets/Scripts/Weapon/SlowMeleeWeapon.cs
+++ TattoBuddy/Assets/Scripts/Weapon/SlowMeleeWeapon.cs
@@ -14,15 +14,33 @@ public class SlowMeleeWeapon : MainWeapon
 
     public override void ShootAtTarget()
     {
-        AttackAnimation();
+        if (currentTarget != null)
+            AttackAnimation();
     }
 
     public void AttackAnimation()
     {
+        var tempWeaponPosition = transform.localPosition;
+        Vector3 newPosition = Vector3.MoveTowards(transform.position, currentTarget.position, 0.15f);
+        transform.position = newPosition;
+        Vector2 direction = currentTarget.position - transform.position;
         float startXRotation = transform.eulerAngles.x;
         float startYRotation = transform.eulerAngles.y;
         float startZRotation = transform.eulerAngles.z;
-        transform.DORotate(new Vector3(startXRotation, startYRotation, startZRotation - 360), 1f, RotateMode.FastBeyond360).SetEase(Ease.Linear).OnComplete(() => ToNormalRotation());
+        transform.rotation = Quaternion.Euler(new Vector3(startXRotation, startYRotation, startZRotation + 90));
+        if (direction.y > 0)
+            transform.DORotate(new Vector3(startXRotation, startYRotation, startZRotation - 120), 0.8f, RotateMode.FastBeyond360).SetEase(Ease.Linear).OnComplete(() =>
+            {
+                ToNormalRotation();
+                transform.localPosition = tempWeaponPosition;
+            });
+        else
+            transform.DORotate(new Vector3(startXRotation, startYRotation, startZRotation - 80), 0.6f).SetEase(Ease.Linear).OnComplete(() =>
+            {
+                ToNormalRotation();
+                transform.localPosition = tempWeaponPosition;
+            });
+
     }
 
     public void ToNormalRotation()
